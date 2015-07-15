4d7057af928f466ceea51820db1e587d5135170b
phase <ph4sesaber@gmail.com>
2015-07-15 11:49:12 -0700

    short keywords

--- JMinusMinus.java
+++ JMinusMinus.java
@@ -9,6 +9,9 @@ import java.util.*;
 public class JMinusMinus {
 
     static HashMap<String, String> replacements = new HashMap<String, String>(){{
+        put("@f", "for");
+        put("@w", "while");
+        put("@i", "if");
         put("main", "public static void main(String[]a)");
         put("fn", "public static");
         put("str", "String");
